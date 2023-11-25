6ad7f04d7400f8fd72ca2feb6e3e7bf549c6f83e
lucasliao0403 <lucasliao0403@gmail.com>
2023-11-25 03:32:21 -0500

    initial commit

---
+++ components/createImage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function CreateImage(props) {
+
+    const image = {
+        id: "i",
+        location: "d",
+        time: "t",
+        
+    }
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CreateImage
